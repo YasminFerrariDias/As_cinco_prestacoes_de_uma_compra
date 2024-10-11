3535e1f023285e2c2a6faf09374896c5a7340364
Yasmin Ferrari Dias <158753555+YasminFerrariDias@users.noreply.github.com>
2024-10-10 21:07:02 -0300

    Add files via upload

---
+++ CincoPrestacoesDeUmaConta.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class CincoPrestacoesDeUmaConta {
+  public static void main(String[] args) {
+    Scanner var = new Scanner(System.in);
+    float compra, prest;
+    System.out.print("Informe o valor total da compra: ");
+    compra = var.nextFloat();
+    prest = compra/5;
+    System.out.print("O valor das cinco prestações sem juros será de " + prest + " reais.");
+  }
+}
